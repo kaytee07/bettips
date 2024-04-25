47bd21f94286712ebd953c281e2b868e435e67d4
kaytee07 <kofi.taylor07@gmail.com>
2024-04-25 12:58:30 +0000

    new email on paystatck

--- components/ui/OddsCard.tsx
+++ components/ui/OddsCard.tsx
@@ -14,15 +14,15 @@ const OddsCard = ({ icon, bgImg, handleClick, title, description}: HomeCardProps
   return (
     <div className={cn("pt-4 flex flex-col bg-cover justify-between w-full  min-h-[260px] rounded-[14px] cursor-pointer", bgImg)}
        onClick={handleClick}>
-        {/* <div className="flex-center glassmorphism size-12
-         rounded-[10px]">
+        <div className="flex-center bg-transparent glassmorphism size-12
+         rounded-[10px] d-">
             <Image
              src={icon}
              alt="meeting"
              width={27}
              height={27}
             />
-        </div> */}
+        </div>
         <div className="flex flex-col px-4 gap-2 bg-[rgba(0,0,0,0.8)]">
             <h1 className="text-xl font-bold ">{title}</h1>
             <p className="text-xl font-bold">{description}</p>
